56e3f3e9be8e2ae93113bd15cec2315e6be2eb92
OutHelix <combo.contra@gmail.com>
2025-03-31 18:00:17 +0300

    initial commit

---
+++ src/test/java/com/outhelix/almis/AlmisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.outhelix.almis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
